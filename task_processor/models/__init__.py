87c65d58184ac87446a860068da543a90c4d0e56
Laurent Constantin <laurent.constantin@liip.ch>
2026-07-10 13:50:27 +0200

    chore: Refactor how models are loaded

--- task_processor/models/__init__.py
+++ task_processor/models/__init__.py
@@ -1,3 +1,4 @@
+from ..constants import GTDConfig, GTDStatus, Priority, ReviewType
 from .api_key import ApiKey
 from .base_models import Area, Context, Tag
 from .document import Document
@@ -5,6 +6,12 @@ from .item import Item
 from .review import ItemStateLog, Review
 
 __all__ = [
+    # Constants and Enums
+    "GTDStatus",
+    "Priority",
+    "ReviewType",
+    "GTDConfig",
+    # Models
     "ApiKey",
     "Context",
     "Area",
